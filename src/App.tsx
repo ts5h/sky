57ce5359fd5199002397f6f18b3ebc1ca4a98169
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-16 22:26:19 +0100

    Classify each file into directories

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.scss'
+import logo from './assets/logo.svg'
+import './scss/App.scss'
 
 const App = () => (
   <div className="App">
