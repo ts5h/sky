7746540f0222f845560cf95b093b7850ed040780
at4 <at4@jc4.so-net.ne.jp>
2023-09-18 22:36:50 +0200

    Add ReturnToHome component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import {ReturnToHome} from "./components/ReturnToHome";
 import { Sky } from "./components/Sky";
 import "./scss/App.scss";
 
 export const App = () => (
   <div className="App">
+    <ReturnToHome />
     <Sky />
   </div>
 );
