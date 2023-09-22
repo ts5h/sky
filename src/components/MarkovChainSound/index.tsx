356e23b32f1faa5dd1faef9270047786f99de8e7
at4 <at4@jc4.so-net.ne.jp>
2023-09-23 01:00:02 +0200

    Add playing note functions

--- src/components/MarkovChainSound/index.tsx
+++ src/components/MarkovChainSound/index.tsx
@@ -1,58 +1,75 @@
-import React, { FC, useCallback, useMemo } from "react";
+import React, { FC, useCallback, useEffect, useMemo, useRef } from "react";
+import Tone from "tone";
+
+Tone.Transport.bpm.value = 90;
+
+const markovChainFreq = [
+  ["C4", "D4", "E4"],
+  ["F4", "G4", "A4"],
+  ["B4", "C5", "D5"],
+];
+
+const markovChainDur = [
+  ["8n", "4n", "2n"],
+  ["16n", "8t", "4n"],
+  ["1m", "2n", "4n"],
+];
+
+const markovChainRest = [
+  0.9, // 10% chance of a rest
+  0.85, // 15% chance of a rest
+  0.8, // 20% chance of a rest
+];
 
 export const MarkovChainSound: FC = () => {
-  const markovChainFreq = useMemo(
-    () => [
-      [0.7, 0.2, 0.1],
-      [0.1, 0.6, 0.2],
-      [0.1, 0.1, 0.8],
-    ],
-    []
-  );
+  const synth = useMemo(() => new Tone.Synth().toDestination(), []);
 
-  const markovChainDur = useMemo(
-    () => [
-      [0.5, 0.3, 0.2],
-      [0.3, 0.4, 0.3],
-      [0.2, 0.3, 0.5],
-    ],
-    []
-  );
+  const currentNoteIndex = useRef(0);
+  const currentDurIndex = useRef(0);
 
-  const markovChainRest = useMemo(
-    () => [
-      [0.9, 0.1], // 10% chance of a rest
-      [0.85, 0.15], // 15% chance of a rest
-      [0.8, 0.2], // 20% chance of a rest
-    ],
-    []
-  );
-
-  const choose = useCallback((arr: number[]): number => {
+  const choose = useCallback((arr: string[]): string => {
     const randIndex = Math.floor(Math.random() * arr.length);
     return arr[randIndex];
   }, []);
 
   const getNextIndex = useCallback(
-    (currentIndex: number, chain: number[][]): number => {
+    (currentIndex: number, chain: string[][]): number => {
       const probs = chain[currentIndex];
       const rand = choose(probs);
       return probs.indexOf(rand);
     },
-    []
+    [choose]
   );
 
-  const getNextNoteIndex = useCallback((currentIndex: number): number => {
-    return getNextIndex(currentIndex, markovChainFreq);
-  }, []);
+  const playNote = useCallback(() => {
+    const noteIndex = getNextIndex(currentNoteIndex.current, markovChainFreq);
+    const nextNote = choose(markovChainFreq[noteIndex]);
 
-  const getNextDurIndex = useCallback((currentIndex: number): number => {
-    return getNextIndex(currentIndex, markovChainDur);
-  }, []);
+    const durIndex = getNextIndex(currentDurIndex.current, markovChainDur);
+    const nextDur = choose(markovChainDur[durIndex]);
 
-  const getNextRestIndex = useCallback((currentIndex: number): number => {
-    return getNextIndex(currentIndex, markovChainRest);
-  }, []);
+    const restIndex = Math.floor(Math.random() * markovChainRest.length);
+    const shouldRest = Math.random() > markovChainRest[restIndex];
 
-  return <>Sound</>;
+    if (!shouldRest) {
+      synth.triggerAttackRelease(nextNote, nextDur);
+    }
+
+    Tone.Transport.scheduleOnce(playNote, `+${Tone.Time(nextDur).toSeconds()}`);
+
+    currentNoteIndex.current = noteIndex;
+    currentDurIndex.current = durIndex;
+  }, [synth, getNextIndex]);
+
+  useEffect(() => {
+    Tone.Transport.start();
+    playNote();
+
+    return () => {
+      Tone.Transport.stop();
+      synth.dispose();
+    };
+  }, [playNote, synth]);
+
+  return null;
 };
