30e8a43b7d27454168446a2264ed1b074029977f
at4 <at4@jc4.so-net.ne.jp>
2023-09-26 22:23:53 +0200

    Update params

--- src/components/MarkovChainSound/index.tsx
+++ src/components/MarkovChainSound/index.tsx
@@ -155,7 +155,7 @@ export const MarkovChainSound: FC = () => {
 
   const playHihat = useCallback(() => {
     if (Math.floor(Math.random() * 8) === 1) {
-      hihatPan.volume.value = -45 + Math.random() * 20;
+      hihatPan.volume.value = -47 + Math.random() * 20;
       hihatPan.pan.value = Math.random() * 2 - 1;
       hihatOsc.start();
       hihatOsc.stop(`+32n`);
